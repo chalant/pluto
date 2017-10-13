41f8771eef68e6433a17e4a9c6f2599d8c9bb224
Peter Harrington <pharrington@g.harvard.edu>
2017-10-13 17:52:53 +0200

    turned off lifetime extension in backtesting
    (cherry picked from commit 8021700)

--- zipline/algorithm_live.py
+++ zipline/algorithm_live.py
@@ -53,6 +53,7 @@ class LiveTradingAlgorithm(TradingAlgorithm):
             kwargs['blotter'] = blotter_live
 
         super(self.__class__, self).__init__(*args, **kwargs)
+        self.asset_finder.is_live = True  # tells the AssetFinder we are live trading
 
         log.info("initialization done")
 
