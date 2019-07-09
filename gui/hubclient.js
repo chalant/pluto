94f30a0096ef6cad918d41f7837de9f1f0ce1f37
Yves Chalant <chalantvsy@gmail.com>
2019-07-09 13:46:53 +0200

    remove gitignore files

---
+++ gui/hubclient.js
@@ -0,0 +1,46 @@
+// var PROTO_PATH = __dirname.replace('gui', 'protos/hub.proto');
+var grpc = require('grpc');
+var protoLoader = require('@grpc/proto-loader');
+// Suggested options for similarity to existing grpc.load behavior
+var packageDefinition = protoLoader.loadSync('./protos/hub.proto', {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true,
+    includeDirs: ['protos']
+});
+
+var protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
+
+const hub = protoDescriptor.hub
+
+class HubClient {
+    //todo: this class should have a reference to the editor => dynamically
+    //displays the strategy in the editor.
+    constructor(editor){
+        this.stub = new hub.Hub('localhost:50051', grpc.credentials.createInsecure())
+        this.currentStr = null //holds the current strategy
+        this.editor = editor
+    }
+
+    //todo: we can actually load all the strategies on the client side.... we don't need to push back a strategy to the hub
+    //each time we want to create a new strategy or modify an existing strategy...
+    viewList() {
+        //displays a list of strategies (collected from the hub)
+        var list = this.stub.List(protoDescriptor.ListRequest()) //request the list of strategies in the hub, and load there
+        //names as 
+        
+        for (var i = 0; i < list; i++) {
+            let div = document.createElement('div')
+            div.innerHTML = 'blabla'
+        }
+    }
+
+    //deploying puts the current branch on top of the branches stack
+    deploy() {
+
+    }
+
+
+}
